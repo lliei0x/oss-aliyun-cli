8b7e789ca5464b668877bfb85724cd97796ea65e
lliei0x <git@liei.cc>
2018-12-05 10:33:02 +0800

    [updata] 增加.gitignore & fresh 监听webserver

--- config/config.go
+++ config/config.go
@@ -4,8 +4,4 @@ const (
 	Endpoint        string = "XXXX"   // oss endpoint
 	AccessKeyID     string = "XXXXX"  // oss accessKeyId
 	AccessKeySecret string = "XXXXXX" // oss secret
-	BucketName      string = "xxx"    // oss bucket名称
-	ObjectName      string = ""       // oss远程目标地址
-	WorkerCount     int    = 100      // 设置最大并发数
-	Suffix          string = ""       // 筛选目录下需要上传的格式
 )
