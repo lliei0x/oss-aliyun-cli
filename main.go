607e1304a441d18d44e2e525bf695c81f16fd943
李磊 <1369183590@qq.com>
2018-12-07 01:09:08 +0800

    [test] CLI工具选择

--- main.go
+++ main.go
@@ -1,46 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"oss-aliyun-cli/config"
-	"oss-aliyun-cli/domain/bucket"
-	"oss-aliyun-cli/domain/object"
-
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
-)
-
-const (
-	path       = `D:\Server\Go\src\oss-aliyun-cli\main.go`
-	bucketName = "leeifme"
-	objectName = ""
+	"oss-aliyun-cli/cmd"
 )
 
 func main() {
-	client, err := oss.New(config.Endpoint, config.AccessKeyID, config.AccessKeySecret)
-	if err != nil {
-		fmt.Println(err)
-	}
-	bucket := &bucket.Bucket{
-		Client: client,
-	}
-	bucket.ListBucket()
-
-	object := &object.Object{
-		Client: client,
-	}
-	// fileinfo, err := os.Stat(path)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(fileinfo.Name())  //获取文件名
-	// fmt.Println(fileinfo.IsDir()) //判断是否是目录，返回bool类型
-	err = object.PutObject(bucketName, path)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// err = object.GetObject(bucketName)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	cmd.Execute()
 }
